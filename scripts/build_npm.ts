b65096c74288fa866b68b8435dc68f72fa1c3d9e
Dilip Kr. Shukla <fusionstrings@gmail.com>
2026-01-06 17:47:16 +0100

    bump v0.1.3

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -18,7 +18,7 @@ await build({
   package: {
     // Must match package.json details
     name: "@fusionstrings/swisseph-wasm",
-    version: "0.1.2",
+    version: "0.1.3",
     description:
       "High-precision Swiss Ephemeris bindings for WebAssembly (Rust based)",
     license: "MIT",
