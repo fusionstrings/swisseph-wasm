e547444c732a0cc0d8c8fd59a634bc03b74381c2
Dilip Kr. Shukla <fusionstrings@gmail.com>
2026-01-06 22:47:26 +0100

    chore: Bump package version to 0.1.4-pre.1 across configuration files.

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -18,7 +18,7 @@ await build({
   package: {
     // Must match package.json details
     name: "@fusionstrings/swisseph-wasm",
-    version: "0.1.3",
+    version: "0.1.4-pre.1",
     description:
       "High-precision Swiss Ephemeris bindings for WebAssembly (Rust based)",
     license: "MIT",
